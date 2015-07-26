6012390fcadfb2370673094775e434ba1ef155b6
xiyang_luo <xylmath@gmail.com>
2015-07-26 17:45:56 -0600

    Sunday

--- Graph/graphgen.m
+++ Graph/graphgen.m
@@ -1,26 +1,19 @@
+function [L,scrop] = graphgen(sh, wsz, psz,neig)
+
 %generate a graph laplacian for each patch with sliding windows
 
 %%%%%%%%%%%%%%%%%% Load Data %%%%%%%%%%%%%%%%%%%%%%%%
 
-%clean image lena
-s_ref = single(rgbtogrey(stdimage('lena')))/255;
-s_ref = s_ref+.1*randn(size(s_ref));
-s_ref = imresize(s_ref,.5);
-[sl,sh] = lowpass(s_ref,5,15);
 
 
 %%%%%%%%%%%%%%%%% Divide the Windows %%%%%%%%%%%%%%%%
 L = {};  
-wsz = [60,60];
-psz = [12,12];
 ind1 = [1,1];
 ind2 = ind1+wsz-[1,1];
-neig = 30;
 k = 1;
 
-while(ind2(1)<=256)
-    while(ind2(2)<= 256)
-        k
+while(ind2(1)<= size(sh,1))
+    while(ind2(2)<= size(sh,2))
         L{k}.ind1 = ind1;
         L{k}.ind2 = ind2;
         a = winlap(sh,ind1,ind2,psz);
@@ -37,22 +30,22 @@ while(ind2(1)<=256)
     ind2 = ind1+wsz-[1,1]; 
 end
 
-%%%%%%%%%%%%%%% Post Processing %%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%% Post Processing %%%%%%%%%%%%%%%%%%%%%%
 x_max = L{end}.ind2(1);
 y_max = L{end}.ind2(2);
 scrop = sh(1:x_max,1:y_max);
-sh = scrop;
-sl = sl(1:x_max,1:y_max);
-
-tag = 'noise';
-save(['data_graph_',tag,'.mat'],'L','sh','sl');
-
-
-x = [];
-for i = 1:16
-    temp = L{i}.phi(:,30);
-    l1 = L{i}.ind1(1):L{i}.ind2(1);
-    r1 = L{i}.ind1(2):L{i}.ind2(2);
-    x(:,:,i) = reshape(temp,length(l1),length(r1));
-end
-square_plot(x,{});
+% sh = scrop;
+% sl = sl(1:x_max,1:y_max);
+% 
+% tag = 'noise';
+% save(['data_graph_',tag,'.mat'],'L','sh','sl');
+% 
+% 
+% x = [];
+% for i = 1:16
+%     temp = L{i}.phi(:,30);
+%     l1 = L{i}.ind1(1):L{i}.ind2(1);
+%     r1 = L{i}.ind1(2):L{i}.ind2(2);
+%     x(:,:,i) = reshape(temp,length(l1),length(r1));
+% end
+% square_plot(x,{});
--- Graph/testcbpdnl.m
+++ Graph/testcbpdnl.m
@@ -1,19 +1,33 @@
-%load precomputed graph info
 
-load('data_graph.mat');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%  Load  Data %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%load a saved noise
+load('noise_data.mat');
+snoise = s;
+% snoise = randn(256,256)*.1;
+s_ref = single(stdimage('lena.grey')) / 255;
+s_ref = imresize(s_ref,.5);
+s = s_ref+snoise;
+wsz = [60,60];
+psz = [12,12];
+neig = 30;
+[sl,sh] = lowpass(s,5,15);
+[L,sh] = graphgen(sh,wsz,psz,neig);
+disp('graph generated');
+sl = sl(1:size(sh,1),1:size(sh,2));
+
 
 % Load dictionary
 load([sporco_path '/Data/ConvDict.mat']);
 dmap = containers.Map(ConvDict.Label, ConvDict.Dict);
 D = dmap('12x12x36');
-sh = sh+.1*randn(size(sh));
 
 
-%set up parameters
-lambda = 0.15;
-mu = 100;
+
+%%%%%%%%%%%%%%%%%%%%%%% set up parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+mu = .6;
 opt = {};
-opt.Verbose = 1;
+opt.Verbose = 0;
 opt.MaxMainIter = 500;
 opt.rho = 100*lambda + 1;
 opt.RelStopTol = 1e-3;
@@ -21,19 +35,62 @@ opt.AuxVarObj = 0;
 opt.HighMemSolve = 1;
 
 %optimize
-[X,~]= cbpdn_L(D,sh,L,lambda,mu,opt);
+lambda = 0.24;
+[Xnl,~]= cbpdn_L(D,sh,L,lambda,mu,opt);
+disp('nl opt');
+lambda = 0.27;
+[Xcn,~] = cbpdn(D,sh,lambda,opt);
+disp('cbpdn');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%% Post processing%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % reconstructing conv
 scnv = @(d,x) ifft2(sum(bsxfun(@times, fft2(d, size(x,1), size(x,2)), ...
                                fft2(x)),3), 'symmetric');
-shrec = scnv(D,X);
+shrecnl = scnv(D,Xnl);
+shreccn = scnv(D,Xcn);
+
+srecnl = shrecnl+sl;
+sreccn = shreccn+sl;
+
+sref = sref(1:size(sh,1),1:size(sh,2));
+
+p = psnr(srecnl,sref);
+disp(['psnr from nonlocal: ',num2str(p)]);
+
+p1 = psnr(sreccn,sref);
+disp(['psnr from cbpdn: ',num2str(p1)]);
 
 figure; 
-imagesc(shrec);
+imagesc(srecnl);
+title(['nonlocal psnr = ',num2str(p)]);
 colorbar;
 
 figure; 
-imagesc((sum3(abs(X))));
+imagesc(sreccn);
+title(['cbpdn psnr = ',num2str(p1)]);
 colorbar;
+
+figure; 
+imagesc(shrecnl);
+title(['nonlocal sh']);
+colorbar;
+
+
+figure; 
+imagesc(shreccn);
+title(['cbpdn sh']);
+colorbar;
+
+figure; 
+imagesc(sum3(Xnl));
+title(['nonlocal coeff']);
+colorbar;
+
+figure; 
+imagesc(sum3(Xcn));
+title(['cbpdn coeff']);
+colorbar;
+
+
+
 
