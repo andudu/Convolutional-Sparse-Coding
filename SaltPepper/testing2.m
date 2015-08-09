9f8c04e05d091326d54ecf4e1d036aea20d3b659
xiyang_luo <xylmath@gmail.com>
2015-08-09 09:56:35 -0600

    foo

--- SaltPepper/testing2.m
+++ SaltPepper/testing2.m
@@ -1,17 +1,28 @@
-% Testing Salt and Pepper denoising
+% Testing image inpainting
 
 % Training images 512 x 512 
 s = single(stdimage('lena.grey'))/255 ;
 
+%generate missing pixels
+ind = randi(512,2,ceil(512*512*.3));
 
-% Add Salt and Pepper
-sn = imnoise(s, 'salt & pepper');
+sn = s;
+for i = 1:size(ind,2)
+    sn(ind(1,i),ind(2,i)) = 1;
+end
+
+
 
 
 % Highpass filter test image
 npd = 16;
 fltlmbd = 5;
-[sl, sh] = lowpass(sn, fltlmbd, npd);
+[sl, sh] = lowpass(s, fltlmbd, npd);
+
+for i = 1:size(ind,2)
+    sh(ind(1,i),ind(2,i)) = 1;
+end
+
 
 
 % Load a standard dictionary
@@ -23,6 +34,11 @@ delta(1,1) = 1;
 D(:,:,end+1) = delta;
 numdict = size(D,3);
 
+opt.L1Weight = ones(512,512,37);
+opt.L1Weight(:,:,37) = 100*ones(512,512);
+for i = 1:size(ind,2)
+    opt.L1Weight(ind(1,i),ind(2,i),37) = 0;
+end
 
 lambda = .03;
 opt.Verbose = 1;
@@ -36,6 +52,10 @@ opt.AutoRhoPeriod = 4;
 
 [X,~] = cbpdn(D,sh,lambda,opt);
 sh_rec = convsum(D,X,1:1:numdict-1);
+
+snoise = convsum(D,X,numdict);
+
+
 % 
 % figure;
 % imagesc(sn);
@@ -47,13 +67,5 @@ sh_rec = convsum(D,X,1:1:numdict-1);
 % axis off;
 % saveas(gcf,'saltpepperreclena','png');
 
-imwrite(sn,'saltpeppernoisylena.png');
-imwrite(sh_rec+sl,'saltpepperreclena.png');
-
-
-
-
-
-
-
-
+imwrite(sn,'iplenaoriginal.png');
+imwrite(sh_rec+sl,'iplenarec.png');
